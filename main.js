5723ae5c76bc78a7d51fe1efcfd5af224b43e27d
arthur <arthurdomiciano46@gmail.com>
2025-02-01 14:18:09 -0500

    slide down and up done

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-$(docunent).ready(function(e){
-    e.preventDefault(e)
-    $('#menu-burger').click(function(){
-    $('ul').slidedown()
+$(document).ready(function(){
+    let clickCount = 0;
+    $('header').on('submit', function(e){
+        e.preventDefault(e)
+        const Span = $('span');
+    })
+    $('#button').click(function() {
+        $('form').slideDown()
+        $('#button').dblclick(function(){
+            $('form').slideUp()
+        })
     })
 })
